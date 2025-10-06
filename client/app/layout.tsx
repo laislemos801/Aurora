967245f5f15f8d0c2fe1579485435c122a2d456c
Lais <laisaplemos10@gmail.com>
2025-10-06 22:50:07 -0100

    initial commit

---
+++ client/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-montserrat',
+  display: 'swap',
+})
+
+export const metadata: Metadata = {
+  title: "Aurora",
+  description: "Gestão de Projeto Semestral Universitário",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" className={montserrat.variable}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ client/firebase/clientApp.ts
@@ -0,0 +1,34 @@
+// lib/clientApp.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+const clientCredentials = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Inicializa Firebase
+const app = initializeApp(clientCredentials);
+
+// Firestore e Auth funcionam tanto no server quanto no client
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Analytics só no browser
+let analytics;
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+
+export { app, db, auth, analytics };
