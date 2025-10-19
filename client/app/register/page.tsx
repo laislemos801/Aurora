34f73a81bb5e02e807a25785810e52c350e28bac
Lais <laisaplemos10@gmail.com>
2025-10-19 13:40:53 -0100

    fix: register components size for notebooks

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -134,14 +134,14 @@ export default function Register() {
         <Image
           src={Horizonte}
           alt="Imagem texto"
-          width={800}
-          height={800}
+          width={700}
+          height={700}
           className="object-contain px-6 hidden 2xl:block"
         />
-        <p className="text-[#FFB97C] text-xl font-semibold mt-1 px-2 leading-none lg:px-6 lg:text-2xl 2xl:text-3xl 2xl:px-30">
+        <p className="text-[#FFB97C] text-xl font-semibold mt-1 px-2 leading-none lg:px-6 lg:text-2xl 2xl:text-3xl">
           <span className='italic'>de inovação</span> <br /> <span className='font-medium'>na integração</span>
         </p>
-        <p className="text-[#C288B3] text-xl font-medium mt-1 px-2 leading-none text-center lg:px-6 lg:text-2xl 2xl:text-3xl 2xl:px-30">
+        <p className="text-[#C288B3] text-xl font-medium mt-1 px-2 leading-none text-center lg:px-6 lg:text-2xl 2xl:text-3xl">
           <span className='font-semibold text-[#4A4A4A]'>e no</span><span className='font-medium italic'> compartilhamento</span>
           <br></br><span className='italic'>de projetos.</span>
         </p>
@@ -171,8 +171,8 @@ export default function Register() {
         <Image
           src={Group2}
           alt="Imagem grupo"
-          width={900}
-          height={900}
+          width={800}
+          height={800}
           className="object-contain"
         />
       </div>
@@ -180,7 +180,7 @@ export default function Register() {
       <div className="flex flex-col z-10">
         
         <div className="relative flex w-80 h-[550px] py-8 border border-white/40 bg-white/25 rounded-[40px] backdrop-blur-sm justify-center
-        md:w-120 md:h-[650px] md:py-12 md:left-8 lg:w-160 xl:h-[700px] 2xl:w-180 2xl:h-[780px] 2xl:left-auto 2xl:mr-10 margin-3xl">
+        md:w-120 md:h-[650px] md:py-12 md:left-8 lg:w-160 xl:h-[680px] 2xl:left-auto 2xl:mr-10 margin-3xl">
 
           <div className="absolute -top-18 left-1/2 -translate-x-1/2">
             <Image
@@ -191,13 +191,13 @@ export default function Register() {
               className="w-33 h-33"
             />
           </div>
-          <div className='flex flex-col 2xl:mt-4'>
+          <div className='flex flex-col'>
             <h1 className={`${lexendExa.className} text-xl text-center text-[#90416B] md:text-[26px] xl:text-[30px]`}>AURORA</h1>
             <h1 className="text-2xl text-center text-[#614281] font-medium mt-2 md:text-3xl xl:text-4xl">Bem-vindo!</h1>
             <p className='text-[10px] text-white text-center px-6 font-light md:text-[12px] md:mt-2 xl:text-[13px] 2xl:text-[14px]'>
               Crie sua conta e faça parte do nascer de uma <br></br> <span className='font-medium italic'>nova forma</span> de integrar e compartilhar ideias.
             </p>
-            <div className='flex flex-col gap-1 mt-4 px-4 md:gap-2 xl:gap-3 xl:mt-6 2xl:mt-9'>
+            <div className='flex flex-col gap-1 mt-4 px-4 md:gap-2 xl:gap-2 xl:mt-6'>
                 <div className="grid w-full max-w-sm items-center gap-1">
                     <Label htmlFor="nome" className='text-[11px] ml-1 text-[#7B6294] md:text-[12px] 2xl:text-[14px]'>Nome</Label>
                     <Input 
