523cb235ef58aa4739bbaec078019631bfa4e6c4
Lais <laisaplemos10@gmail.com>
2025-11-01 18:19:21 -0200

    create groupinfo page

---
+++ client/app/group-info/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import 'primeicons/primeicons.css';
+        
+
+export default function GroupInfo() {
+
+    return (
+        <div className="px-8">
+            <i className="pi pi-chevron-circle-left"></i>
+            <p>Grupo 1</p>
+
+
+
+
+        </div>
+    );
+
+}
