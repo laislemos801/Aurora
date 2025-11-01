3dd6fe8c809cf44d7990e5ded2b6ad78c36c5c7f
Lais <laisaplemos10@gmail.com>
2025-11-01 21:56:46 -0200

    add sidebar for mobile

--- client/components/toolBar/left.tsx
+++ client/components/toolBar/left.tsx
@@ -7,10 +7,12 @@ import icon from "@/public/for_you.svg";
 import logo from "@/public/logo.png";
 import { MdExitToApp } from "react-icons/md";
 import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ToolBarLeft() {
   const pathname = usePathname();
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="bg-[#FCF3FA] w-full h-full flex flex-col justify-between items-center pb-8">
@@ -18,9 +20,8 @@ export default function ToolBarLeft() {
       <div className="hidden sm:block sm:w-23 -mt-1 xl:w-26">
         <Image src={logo} alt={"logo"} width={1920} height={1080} />
       </div>
-
-     
-      <div className="hidden sm:flex flex-col gap-8 items-center text-[#C288B3] 2xl:gap-11">
+ 
+      <div className="hidden sm:flex flex-col gap-12 items-center text-[#C288B3]">
         <div
           className={`${
             pathname === "/" && "bg-[#90416B] text-white p-1.5 rounded-full 2xl:p-2"
