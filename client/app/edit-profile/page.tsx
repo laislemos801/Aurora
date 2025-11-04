ed50181d3782e1c7e82fdc431e1cbebcfcd50c57
annacarolinma <carolinamorellichaves@gmail.com>
2025-11-04 01:11:52 -0300

    style: component dashboard

--- client/app/edit-profile/page.tsx
+++ client/app/edit-profile/page.tsx
@@ -3,7 +3,7 @@
 import "primeicons/primeicons.css";
 import HeaderPicture from "@/components/edit-profile/headerPicture";
 import PersonalInfo from "@/components/edit-profile/personalInfo";
-import Projects from "@/components/edit-profile/projects";
+import Dashboard from "@/components/edit-profile/dashboards";
 
 
 export default function EditProfile() {
@@ -15,7 +15,7 @@ export default function EditProfile() {
       {/* conteúdo da tela abaixo */}
       <div className="flex flex-col rounded-t-2xl mt-6">
         <PersonalInfo/>
-        <Projects/>
+        <Dashboard/>
       </div>
     </div>
   );
--- client/components/edit-profile/dashboards.tsx
+++ client/components/edit-profile/dashboards.tsx
@@ -2,15 +2,15 @@
 
 import React from "react";
 
-export default function Projects() {
+export default function Dashboard() {
   return (
     <div className="w-full flex flex-col justify-start items-start sm:px-12 md:px-16 mt-2 p-6">
       <h1 className="text-2xl sm:text-3xl font-medium text-[#000000]">
-        Seus projetos gerenciados
+        Seus dashboards
       </h1>
 
       <p className="text-sm text-[#C9AFC8] mt-2">
-        projetos atrelados ao professor
+        dashboards de projeto
       </p>
     </div>
   );
